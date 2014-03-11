c1374f7a801b0f9093318a1e3876a76173894883
myrie <myriegnome@gmail.com>
2014-03-11 07:18:31 -0700

    things

--- Linedrawing/LineDrawing/LineDrawing.java
+++ Linedrawing/LineDrawing/LineDrawing.java
@@ -156,7 +156,7 @@ public class LineDrawing implements RasterListener {
 
         boolean swap = Math.abs(endRow - startRow) > Math.abs(endCol - startCol);
 
-		// swap x and y if needed, to make sure that the slope is less than 1
+        // swap x and y if needed, to make sure that the slope is less than 1
         // Why is this necessary?
         if (swap) {
             int save;
@@ -212,52 +212,87 @@ public class LineDrawing implements RasterListener {
      * 
      */
     private void drawDDALine(int startCol, int startRow, int endCol, int endRow) {
-        boolean swap = Math.abs(endRow - startRow) > Math.abs(endCol - startCol);
-
-		// swap x and y if needed, to make sure that the slope is less than 1
-        // Why is this necessary?
-        if (swap) {
-            int save;
-
-            save = startCol;
-            startCol = startRow;
-            startRow = save;
-
-            save = endCol;
-            endCol = endRow;
-            endRow = save;
-        }
-
         // the slope
-        double m = (endRow - startRow) / (double) (endCol - startCol);
+//        double m = (endRow - startRow) / (double) (endCol - startCol);
+//        double y = startRow;
+//        double x = startCol;
+//        if (Math.abs(m) <= 1){    
+//            double xInc = (startCol < endCol) ? 1 : -1;
+//            raster.putPixel((int) x, (int) y);
+//            while (x != endCol)
+//            {
+//                x += xInc;
+//                y += m*xInc;
+//
+//                raster.putPixel((int) x, (int) y);
+//
+//            }
+//        } else {
+//            double yInc = (startRow < endRow) ? 1 : -1;
+//            double invertM = 1.0/m;
+//            raster.putPixel((int) x, (int) y);
+//            while (y != endRow)
+//            {
+//                y += yInc;
+//                x = x + invertM*yInc;
+//                raster.putPixel((int) x, (int) y);
+//
+//            } 
+//        }
+        
+        drawDDATest(startCol, startRow, endCol, endRow);
+    }
+    
+    private void drawDDATest(int x1, int y1, int x2, int y2){
+        double dx = x2-x1;
+        double dy = y2-y1;
 
-        // handles left-to-right or right-to-left
-        int xInc = (startCol < endCol) ? 1 : -1;
+        if (Math.abs(y2 - y1) <= Math.abs(x2 - x1)) {
 
-        // fill in the starting pixel
-        int col = startCol;
+            if ((x1 == x2) && (y1 == y2)) {
+                raster.putPixel(x1, y1);
 
-        if (swap) {
-            raster.putPixel(startRow, col);
-        } else {
-            raster.putPixel(col, startRow);
-        }
-
-        // now step along, calculating the y value for each x value as we go
-        while (col != endCol) {
-            col += xInc;
-            // here's the other half of the swap x and y trick
-            if (swap) {
-                raster.putPixel(
-                        startRow + (int) Math.round(m * (col - startCol)),
-                        col
-                );
             } else {
-                raster.putPixel(
-                        col,
-                        startRow + (int) Math.round(m * (col - startCol))
-                );
+                if (x2 < x1) {
+                    int tmp = x2;
+                    x2 = x1;
+                    x1 = tmp;
+
+                    tmp = y2;
+                    y2 = y1;
+                    y1 = tmp;
+                }
+
+                double k = (double)dy/dx; 
+                int cele_y;           
+                double y = (double)y1;
+
+                for (int x = x1 ; x <= x2 ; x++) {
+                    cele_y = (int)Math.round(y);
+                    raster.putPixel(x, cele_y);
+                    y += k;
+                }
             }
+        } else {
+
+                if (y2 < y1) {
+                    int tmp = x2;
+                    x2 = x1;
+                    x1 = tmp;
+
+                    tmp = y2;
+                    y2 = y1;
+                    y1 = tmp;
+                }
+
+                double k = (double)dx/dy;
+                int cele_x;
+                double x = (double)x1;
+                for (int y = y1; y <= y2; y++) {
+                    cele_x = (int)Math.round(x);
+                    raster.putPixel(cele_x, y);
+                    x += k;
+                }
         }
     }
 
