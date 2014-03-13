802ca83f84f03eedffe5f90fc5255abfbe49c3b7
Maligothez <genericdragonm@gmail.com>
2014-03-13 10:42:46 +0800

    Mit Breshenham

--- Linedrawing/LineDrawing/LineDrawing.java
+++ Linedrawing/LineDrawing/LineDrawing.java
@@ -247,8 +247,38 @@ public class LineDrawing implements RasterListener {
     }
 
     private void drawBresenhamLine(int x1, int y1, int x2, int y2) {
-    }
+        
+{
+	int dx = Math.abs(x2 - x1);
+        int dy = Math.abs (y2 - y1);
+	int p = 2 * dy - dx;
+	int twoDy = 2 * dy, twoDyMinusDx = 2 * (dy - dx);
+	int x, y;
 
+	/* Determine which endpoint to use as start position */
+	if(x1 > x2) {
+		x = x2;
+		y = y2;
+		x2 = x1;
+	}
+	else {
+		x = x1;
+		y = y1;
+	}
+
+	raster.putPixel(x, y);
+	while (x < x2) {
+		x++;
+		if(p < 0)
+			p += twoDy;
+		else {
+			y++;
+			p += twoDyMinusDx;
+		}
+		raster.putPixel(x, y);
+	}
+}
+    }
     // RasterListener methods
     // used for rubberband effect
     /*
